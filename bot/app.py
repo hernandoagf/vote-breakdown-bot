ec3fe3725307a9570b142ba459df58ea515492c3
hernandoagf <hernando.guzman93@gmail.com>
2022-11-17 16:35:29 -0300

    Display active votes

--- bot/app.py
+++ bot/app.py
@@ -4,6 +4,7 @@ from discord import app_commands
 import os
 from dotenv import load_dotenv
 import logging
+from datetime import datetime, timezone
 from helpers import *
 
 load_dotenv()
@@ -23,22 +24,70 @@ intents = discord.Intents.default()
 client = MyClient(intents=intents)
 
 
+class VotesEmbed(discord.Embed):
+    def __init__(self, image, footer_text=None):
+        super().__init__(
+            title="Active votes",
+            description="Vote breakdown for currently active on-chain polls and executive votes",
+            color=0xF4B731,
+            timestamp=datetime.now(timezone.utc),
+        )
+        self.set_image(url=image)
+        self.set_footer(text=footer_text)
+
+
 class NavigationRow(discord.ui.View):
-    def __init__(self):
+    def __init__(self, results):
         super().__init__()
         self.value = None
+        self.results = results
+        self.page = 0
 
-    @discord.ui.button(emoji="⬅️", disabled=True)
+    @discord.ui.button(emoji="⬅️", disabled=True, custom_id="previous_button")
     async def navigate_previous(
         self, interaction: discord.Interaction, button: discord.ui.Button
     ):
-        await interaction.response.edit_message(content="Previous page", attachments=[])
+        self.page -= 1
+        if self.children[1].disabled:
+            self.children[1].disabled = False
+        button.disabled = True if self.page == 0 else False
+        items_displayed = (self.page * 3, (self.page * 3) + 3)
 
-    @discord.ui.button(emoji="➡️")
+        generate_progress_bar(self.results[(items_displayed[0]) : (items_displayed[1])])
+        progress_bar_file = discord.File("progress_bars.png")
+
+        await interaction.response.edit_message(
+            attachments=[progress_bar_file],
+            embed=VotesEmbed(
+                image="attachment://progress_bars.png",
+                footer_text=f"Displaying {items_displayed[0] + 1}-{items_displayed[1]} of {len(self.results)} active votes",
+            ),
+            view=self,
+        )
+
+    @discord.ui.button(emoji="➡️", disabled=False, custom_id="next_button")
     async def navigate_next(
         self, interaction: discord.Interaction, button: discord.ui.Button
     ):
-        await interaction.response.edit_message(content="Next page", attachments=[])
+        self.page += 1
+        if self.children[0].disabled:
+            self.children[0].disabled = False
+        button.disabled = True if (self.page * 3) + 3 >= len(self.results) else False
+        items_displayed = (
+            self.page * 3,
+            len(self.results) if button.disabled else (self.page * 3) + 3,
+        )
+        generate_progress_bar(self.results[items_displayed[0] : items_displayed[1]])
+        progress_bar_file = discord.File("progress_bars.png")
+
+        await interaction.response.edit_message(
+            attachments=[progress_bar_file],
+            embed=VotesEmbed(
+                image="attachment://progress_bars.png",
+                footer_text=f"Displaying {items_displayed[0] + 1}-{items_displayed[1]} of {len(self.results)} active votes",
+            ),
+            view=self,
+        )
 
 
 @client.event
@@ -52,20 +101,26 @@ async def votes(interaction: discord.Interaction):
 
     await interaction.response.defer()
 
-    active_polls = get_active_polls()
-    results = get_polls_results(active_polls)
-    polls_length = len(results)
+    active_votes = get_active_votes()
+    results = get_votes_results(active_votes)
+    votes_length = len(results)
 
-    generate_progress_bar(results if polls_length <= 3 else results[0:3])
+    generate_progress_bar(results if votes_length <= 3 else results[0:3])
     progress_bar_file = discord.File("progress_bars.png")
 
-    if polls_length <= 3:
-        await interaction.followup.send(file=progress_bar_file)
+    if votes_length <= 3:
+        await interaction.followup.send(
+            file=progress_bar_file,
+            embed=VotesEmbed(image="attachment://progress_bars.png"),
+        )
     else:
         await interaction.followup.send(
-            f"Displaying 1-3 of {polls_length} votes",
             file=progress_bar_file,
-            view=NavigationRow(),
+            embed=VotesEmbed(
+                image="attachment://progress_bars.png",
+                footer_text=f"Displaying 1-3 of {votes_length} active votes",
+            ),
+            view=NavigationRow(results=results),
         )
 
 
--- bot/helpers.py
+++ bot/helpers.py
@@ -1,10 +1,67 @@
 import requests
 from locale import LC_ALL, setlocale, format_string
-from datetime import datetime as dt
-from pytz import timezone
+from datetime import datetime as dt, timezone
 from PIL import Image, ImageDraw, ImageFont
+from math import floor
 
 setlocale(LC_ALL, "en_US")
+
+
+def get_active_votes():
+    """Gets poll and executive vote IDs"""
+
+    poll_response = requests.get("https://vote.makerdao.com/api/polling/all-polls")
+    executive_response = requests.get("https://vote.makerdao.com/api/executive?limit=5")
+    poll_data = poll_response.json()
+    executive_data = executive_response.json()
+    hat = next(e for e in executive_data if e["spellData"]["hasBeenCast"])
+
+    current_date = dt.now(timezone.utc)
+    active_polls = list(
+        filter(
+            lambda poll: dt.strptime(poll["endDate"], "%Y-%m-%dT%H:%M:%S.%f%z")
+            > current_date,
+            poll_data["polls"],
+        )
+    )
+    active_executives = list(
+        filter(
+            lambda executive: executive["active"],
+            executive_data,
+        )
+    )
+
+    parsed_polls = sorted(
+        list(
+            map(
+                lambda poll: {"id": poll["pollId"], "title": poll["title"]},
+                active_polls,
+            )
+        ),
+        key=lambda poll: poll["id"],
+        reverse=True,
+    )
+    parsed_executives = sorted(
+        list(
+            map(
+                lambda executive: {
+                    "id": executive["address"],
+                    "title": executive["title"],
+                    "created_at": dt.strptime(
+                        executive["date"],
+                        "%a %b %d %Y %H:%M:%S %Z%z (Coordinated Universal Time)",
+                    ),
+                    "mkr_support": float(executive["spellData"]["mkrSupport"]) / 1e18,
+                    "mkr_on_hat": float(hat["spellData"]["mkrSupport"]) / 1e18,
+                },
+                active_executives,
+            )
+        ),
+        key=lambda executive: executive["created_at"],
+        reverse=True,
+    )
+
+    return [*parsed_executives, *parsed_polls]
 
 
 def get_active_polls():
@@ -13,7 +70,7 @@ def get_active_polls():
     response = requests.get("https://vote.makerdao.com/api/polling/all-polls")
     data = response.json()
 
-    current_date = dt.now(timezone("UTC"))
+    current_date = dt.now(timezone.utc)
     active_polls = list(
         filter(
             lambda poll: dt.strptime(poll["endDate"], "%Y-%m-%dT%H:%M:%S.%f%z")
@@ -26,7 +83,7 @@ def get_active_polls():
         list(
             map(
                 lambda poll: {"id": poll["pollId"], "title": poll["title"]},
-                active_polls[0:5],
+                active_polls[0:12],
             )
         ),
         key=lambda poll: poll["id"],
@@ -64,29 +121,32 @@ def get_poll_votes(poll_id: str):
     return poll_results
 
 
-def get_polls_results(polls):
-    """Gets the vote breakdown for multiple polls"""
+def get_votes_results(votes):
+    """Gets the vote breakdown for multiple votes"""
 
-    for poll in polls:
-        response = requests.get(
-            f"https://vote.makerdao.com/api/polling/tally/{poll['id']}"
-        )
-        data = response.json()
-        poll["type"] = data["parameters"]["inputFormat"]["type"]
-        poll["results"] = list(
-            map(
-                lambda result: {
-                    "option_name": result["optionName"],
-                    "mkr_support": float(result["mkrSupport"]),
-                    "percentage": result["firstPct"] / 100,
-                },
-                data["results"],
+    for vote in votes:
+        if str(vote["id"]).startswith("0x"):
+            continue
+        else:
+            response = requests.get(
+                f"https://vote.makerdao.com/api/polling/tally/{vote['id']}"
             )
-        )
-    return polls
+            data = response.json()
+            vote["type"] = data["parameters"]["inputFormat"]["type"]
+            vote["results"] = list(
+                map(
+                    lambda result: {
+                        "option_name": result["optionName"],
+                        "mkr_support": float(result["mkrSupport"]),
+                        "percentage": result["firstPct"] / 100,
+                    },
+                    data["results"],
+                )
+            )
+    return votes
 
 
-def generate_progress_bar(poll_votes):
+def generate_progress_bar(votes):
     """Generates the vote breakdown progress bars"""
 
     colors = ["#5bbeae", "#dc7d39", "#7e7e86"]
@@ -95,57 +155,60 @@ def generate_progress_bar(poll_votes):
     option_height = 30
     border_radius = 5
     canvas_width, canvas_height = (
-        margin + (bar_width + margin) * len(poll_votes),
+        margin + (bar_width + margin) * len(votes),
         option_height
         * len(
             max(
-                map(lambda poll: poll["results"], poll_votes),
-                key=lambda poll: len(poll),
+                map(lambda vote: vote.get("results", []), votes),
+                key=lambda vote: len(vote),
             )
         )
         + 40
         + margin * 2,
     )
     bar_background = (15, 15, 15)
+    img_background = (47, 49, 54)
     s_font = ImageFont.truetype("fonts/Roboto-Regular.ttf", 12)
     m_font = ImageFont.truetype("fonts/Roboto-Regular.ttf", 14)
 
     # Create an image
-    img = Image.new("RGB", (canvas_width, canvas_height), (55, 57, 62))
+    img = Image.new("RGB", (canvas_width, canvas_height), img_background)
 
     # Get a drawing context
     draw = ImageDraw.Draw(img)
 
-    for j, poll in enumerate(poll_votes):
-        draw.text(
-            (margin + (margin + bar_width) * j, margin),
-            f"Poll {poll.get('id')}:",
-            font=m_font,
-        )
-        poll_title = poll.get("title")
-        while draw.textlength(poll_title, m_font) > bar_width:
-            poll_title = f"{poll_title[:-2]}…"
-        draw.text(
-            (margin + (margin + bar_width) * j, margin + 16), poll_title, font=m_font
-        )
+    for j, vote in enumerate(votes):
+        if str(vote["id"]).startswith("0x"):
+            bar_y = margin + 45
 
-        for i, result in enumerate(poll.get("results")):
-            bar_y = margin + 45 + (option_height * i)
-            bar_fill_width = bar_width * result.get("percentage", 0)
-
-            option_name = result.get("option_name")
-            while draw.textlength(option_name, s_font) > bar_width / 2:
-                option_name = f"{option_name[:-2]}…"
             draw.text(
-                (margin + (margin + bar_width) * j, bar_y), option_name, font=s_font
+                (margin + (margin + bar_width) * j, margin),
+                f"Executive {vote['id'][0:6]}...{vote['id'][-4:]}:",
+                font=m_font,
+            )
+            spell_title = vote.get("title")
+            while draw.textlength(spell_title, m_font) > bar_width:
+                spell_title = f"{spell_title[:-2]}…"
+            draw.text(
+                (margin + (margin + bar_width) * j, margin + 16),
+                spell_title,
+                font=m_font,
             )
 
             draw.text(
+                (margin + (margin + bar_width) * j, bar_y),
+                f"{format_string('%d', vote['mkr_support'], grouping=True)} MKR ({floor((vote['mkr_support'] / vote['mkr_on_hat']) * 100)}%)",
+                font=s_font,
+            )
+            draw.text(
                 (margin + (margin + bar_width) * j + bar_width, bar_y),
-                f'{format_string("%10.0f", result.get("mkr_support"), grouping=True)} MKR - {round(result.get("percentage") * 100)}%',
+                f"{format_string('%d', vote['mkr_on_hat'] - vote['mkr_support'], grouping=True)} more needed"
+                if vote["mkr_on_hat"] - vote["mkr_support"] > 0
+                else "",
                 font=s_font,
                 anchor="ra",
             )
+
             # Draw the background
             draw.rounded_rectangle(
                 (
@@ -158,18 +221,75 @@ def generate_progress_bar(poll_votes):
                 radius=border_radius,
             )
             # Draw the progress bar
-            if result.get("percentage", 0) != 0:
+            draw.rounded_rectangle(
+                (
+                    margin + (margin + bar_width) * j,
+                    bar_y + 16,
+                    margin
+                    + (margin + bar_width) * j
+                    + (bar_width * (vote["mkr_support"] / vote["mkr_on_hat"])),
+                    bar_y + 16 + bar_height,
+                ),
+                fill="#f4b731",
+                radius=border_radius,
+            )
+
+        else:
+            draw.text(
+                (margin + (margin + bar_width) * j, margin),
+                f"Poll {vote.get('id')}:",
+                font=m_font,
+            )
+            poll_title = vote.get("title")
+            while draw.textlength(poll_title, m_font) > bar_width:
+                poll_title = f"{poll_title[:-2]}…"
+            draw.text(
+                (margin + (margin + bar_width) * j, margin + 16),
+                poll_title,
+                font=m_font,
+            )
+
+            for i, result in enumerate(vote.get("results")):
+                bar_y = margin + 45 + (option_height * i)
+                bar_fill_width = bar_width * result.get("percentage", 0)
+
+                option_name = result.get("option_name")
+                while draw.textlength(option_name, s_font) > bar_width / 2:
+                    option_name = f"{option_name[:-2]}…"
+                draw.text(
+                    (margin + (margin + bar_width) * j, bar_y), option_name, font=s_font
+                )
+
+                draw.text(
+                    (margin + (margin + bar_width) * j + bar_width, bar_y),
+                    f'{format_string("%10.0f", result.get("mkr_support"), grouping=True)} MKR - {round(result.get("percentage") * 100)}%',
+                    font=s_font,
+                    anchor="ra",
+                )
+                # Draw the background
                 draw.rounded_rectangle(
                     (
                         margin + (margin + bar_width) * j,
                         bar_y + 16,
-                        margin + (margin + bar_width) * j + bar_fill_width,
+                        margin + (margin + bar_width) * j + bar_width,
                         bar_y + 16 + bar_height,
                     ),
-                    fill=colors[0]
-                    if poll.get("type") != "single-choice"
-                    else colors[i],
+                    fill=bar_background,
                     radius=border_radius,
                 )
+                # Draw the progress bar
+                if result.get("percentage", 0) != 0:
+                    draw.rounded_rectangle(
+                        (
+                            margin + (margin + bar_width) * j,
+                            bar_y + 16,
+                            margin + (margin + bar_width) * j + bar_fill_width,
+                            bar_y + 16 + bar_height,
+                        ),
+                        fill=colors[0]
+                        if vote.get("type") != "single-choice"
+                        else colors[i],
+                        radius=border_radius,
+                    )
 
     img.save("progress_bars.png", "PNG")
